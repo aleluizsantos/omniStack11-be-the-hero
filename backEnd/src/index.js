cb2f0e5f5e9e753ac94506bb5ac7996cde60ebdf
Alessandro L. Santos <aleluizsantos@gmail.com>
2020-03-25 10:33:45 -0300

    Backend application

---
+++ backEnd/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+//Informar que estaremos utilizando JSON para as requisições
+app.use(express.json()); 
+
+/*
+* TIPOS DE PARAMETROS
+* Query Params: Parâmetros nomeados enviados na rota após "?"  (Filtros e paginação)
+* Query Params: Parâmetros utilizados para identificar recursos
+* Request Body: Corpo da requisição, utilizdo para criar e alterar recursos
+*/
+
+/**
+ * TIPO DE BANCO DE DADOS
+ * SQL: MYSQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, ClouchDB
+ */
+/**
+ * PARA MANIPULAR BANCO DE DADOS DUAS FORMAS
+ * 1) Instalando os DRIVE de qual arquitetura que deseja usar
+ *    ex: SELECT * FROM users
+ * 2) Usar uma Query Builder por ser o ==>> KNEX.JS
+ *    ex: table('users').select('*').where(...)...
+ */
+
+app.use(routes);
+
+app.listen(3333);
